5d481fd689f14caf4bbc00ff90607f40ee095044
keithchasen <keithchasen89@gmail.com>
2025-06-25 22:13:29 +0200

    basic types

---
+++ Pages/basicTypes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+let item: String = "Hello, World!"
+
+let title = item
+
+let someBool: Bool = true
+
+let someDate: Date = Date()
+
+let someNumber: Int = 42
+
+// use for calculations
+let someFloat: Float = 42.0
+
+// use for UI
+let cgFloat: CGFloat = 42.0
+
